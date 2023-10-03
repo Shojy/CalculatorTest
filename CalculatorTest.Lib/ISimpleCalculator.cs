8b92e6de418b4a2c09a98cc80a26005a957355e8
Joshua Moon <joshua@moon.uk.net>
2023-10-03 15:57:48 +0100

    Add project files.

---
+++ CalculatorTest.Lib/ISimpleCalculator.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+
+namespace CalculatorTest.Lib;
+
+[UsedImplicitly]
+public interface ISimpleCalculator
+{
+    int Add(int start, int amount);
+    int Subtract(int start, int amount);
+}
+
+[UsedImplicitly]
+public class SimpleCalculator : ISimpleCalculator
+{
+    public int Add(int start, int amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Subtract(int start, int amount)
+    {
+        throw new NotImplementedException();
+    }
+}
