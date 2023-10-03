2675937af76f524fa64ca423eed259866917c7ca
Joshua Moon <joshua@moon.uk.net>
2023-10-03 16:43:06 +0100

    Add implementation to methods

--- CalculatorTest.Lib/ISimpleCalculator.cs
+++ CalculatorTest.Lib/ISimpleCalculator.cs
@@ -5,20 +5,38 @@ namespace CalculatorTest.Lib;
 [UsedImplicitly]
 public interface ISimpleCalculator
 {
+    /// <summary>
+    /// Add 2 numbers together and return the result
+    /// </summary>
+    /// <param name="start"></param>
+    /// <param name="amount"></param>
+    /// <returns>The sum of start and amount</returns>
     int Add(int start, int amount);
+
+    /// <summary>
+    /// Subtracts an amount from the starting value
+    /// </summary>
+    /// <param name="start">The starting value</param>
+    /// <param name="amount">The amount to subtract</param>
+    /// <returns></returns>
     int Subtract(int start, int amount);
 }
 
 [UsedImplicitly]
 public class SimpleCalculator : ISimpleCalculator
 {
+    [Pure]
     public int Add(int start, int amount)
     {
-        throw new NotImplementedException();
+        return start + amount;
     }
 
+    [Pure]
     public int Subtract(int start, int amount)
     {
-        throw new NotImplementedException();
+        // In some scenarios, subtracting a negative number may wish to result in an error. In this
+        // case, it is assumed that negative values are acceptable and thus treated normally without
+        // a filter or error.
+        return start - amount;
     }
 }
