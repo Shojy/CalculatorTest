6c046e3fd7f6d53cab6661b80c41ae40ad60a5fe
Joshua Moon <joshua@moon.uk.net>
2023-10-04 13:27:55 +0100

    Refactor to top level namespace

--- CalculatorTest.Api/Controllers/CalculatorController.cs
+++ CalculatorTest.Api/Controllers/CalculatorController.cs
@@ -2,38 +2,39 @@ using CalculatorTest.Api.Models;
 using CalculatorTest.Lib;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CalculatorTest.Api.Controllers
+namespace CalculatorTest.Api.Controllers;
+
+[ApiController]
+
+[Route("api/[controller]")]
+public class CalculatorController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class CalculatorController : ControllerBase
+
+    private readonly ILogger<CalculatorController> _logger;
+    private readonly ISimpleCalculator _calculator;
+
+    public CalculatorController(ILogger<CalculatorController> logger, ISimpleCalculator calculator)
     {
+        _logger = logger;
+        _calculator = calculator;
+    }
 
-        private readonly ILogger<CalculatorController> _logger;
-        private readonly ISimpleCalculator _calculator;
+    [HttpGet("[action]")]
+    public CalculatorResultDto Add(int start, int amount)
+    {
+        _logger.LogDebug("Entering {class}.{method}", nameof(CalculatorController), nameof(Add));
+        var result = _calculator.Add(start, amount);
 
-        public CalculatorController(ILogger<CalculatorController> logger, ISimpleCalculator calculator)
-        {
-            _logger = logger;
-            _calculator = calculator;
-        }
+        return new CalculatorResultDto(result);
+    }
 
-        [HttpGet("[action]")]
-        public CalculatorResultDto Add(int start, int amount)
-        {
-            _logger.LogDebug("Entering {class}.{method}", nameof(CalculatorController), nameof(Add));
-            var result = _calculator.Add(start, amount);
+    [HttpGet("[action]")]
+    public CalculatorResultDto Subtract(int start, int amount)
+    {
+        _logger.LogDebug("Entering {class}.{method}", nameof(CalculatorController), nameof(Subtract));
+        var result = _calculator.Subtract(start, amount);
 
-            return new CalculatorResultDto(result);
-        }
-
-        [HttpGet("[action]")]
-        public CalculatorResultDto Subtract(int start, int amount)
-        {
-            _logger.LogDebug("Entering {class}.{method}", nameof(CalculatorController), nameof(Subtract));
-            var result = _calculator.Subtract(start, amount);
-
-            return new CalculatorResultDto(result);
-        }
+        return new CalculatorResultDto(result);
     }
 }
+
